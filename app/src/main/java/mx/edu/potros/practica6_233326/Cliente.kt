5001a0fb476cb9a12c906469c1bc6dbd2d64ce7f
AnthX0 <victorglezb@gmail.com>
2024-03-05 16:28:16 -0700

    Nuevo Commit Práctica 7

---
+++ app/src/main/java/mx/edu/potros/practica6_233326/Cliente.kt
@@ -0,0 +1,5 @@
+package mx.edu.potros.practica6_233326
+
+data class Cliente(var nombre: String,
+                   var tipoPago: String,
+                   var asiento: Int)
--- app/src/main/java/mx/edu/potros/practica6_233326/detalle_pelicula.kt
+++ app/src/main/java/mx/edu/potros/practica6_233326/detalle_pelicula.kt
@@ -1,5 +1,6 @@
 package mx.edu.potros.practica6_233326
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
@@ -10,15 +11,37 @@ class detalle_pelicula : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalle_pelicula)
 
+        val bundle = intent.extras
         val tv_movie_image: ImageView = findViewById(R.id.tv_movie_image)
         val tv_movie_name: TextView = findViewById(R.id.tv_movie_name)
         val tv_movie_desc: TextView = findViewById(R.id.tv_movie_desc)
+        val seatsLeft: TextView = findViewById(R.id.seatLeft)
+        val buyTickets: TextView = findViewById(R.id.buyTickets)
+        var ns = 0
+        var id = -1
+        var title = ""
 
-        val bundle = intent.extras
         if (bundle != null) {
+            ns = bundle.getInt("numberSeats")
+            title = bundle.getString("titulo")!!
             tv_movie_image.setImageResource(bundle.getInt("header"))
             tv_movie_name.text = bundle.getString("titulo")
             tv_movie_desc.text = bundle.getString("sinopsis")
+            seatsLeft.setText("$ns seats available")
+            id = bundle.getInt("pos")
+        }
+
+        if (ns == 0) {
+            buyTickets.isEnabled = false
+        } else {
+            buyTickets.setOnClickListener {
+                buyTickets.isEnabled = true
+                val intent: Intent = Intent(this,
+                    SeatSelection::class.java)
+                intent.putExtra("id", id)
+                intent.putExtra("name", title)
+                this.startActivity(intent)
+            }
         }
     }
 }
